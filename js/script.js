f0fcbecb427505224a10eec8d9cf7ffe4a66d694
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-07-17 20:31:19 +0300

    fix

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 //Реалізуйте функцію generateKey(length, characters), яка повертає рядок випадкових символів із набору characters довжиною length. span>
 
 const inputLength = +prompt('Введіть довижу')
-const randomChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+const randomChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 const generateKey = function (length, characters){
     let result = '';
     for (let i =0; i < length; i++){
-        result += characters.charAt(Math.floor(Math.random() * length));
+        result += characters.charAt(Math.random() * length);
+        console.log(result)
     }
     return result
 }
