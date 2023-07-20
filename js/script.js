cdecc2a3489f846e32f127d2a92bbdeb20d2769d
Dmytro-Foliushniak <126951155+Dmytro-Foliushniak@users.noreply.github.com>
2023-07-20 11:28:53 +0300

    dell comment

--- js/script.js
+++ js/script.js
@@ -1,13 +1,5 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
-// const removeElement = function (arr, deleteNumber){
-//     for (let i = 0; i < arr.length; i++){
-//         if (arr[i] === deleteNumber){
-//
-//         }
-//     }
-// }
-
 const removeElement = function (arr, deleteNumber){
     arr.splice(arr.indexOf(deleteNumber), 1)
     return arr
