945897a6ee15a45e323a001b4f577dce760c83eb
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-07-20 11:28:12 +0300

    fix logic

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 const array = [1, 2, 3, 4, 5, 6, 7];
 
+// const removeElement = function (arr, deleteNumber){
+//     for (let i = 0; i < arr.length; i++){
+//         if (arr[i] === deleteNumber){
+//
+//         }
+//     }
+// }
+
 const removeElement = function (arr, deleteNumber){
-    for (let i = 0; i < arr.length; i++){
-        if (arr[i] === deleteNumber){
-            arr.splice(arr.indexOf(arr[i]), 1)
-        }
-    }
+    arr.splice(arr.indexOf(deleteNumber), 1)
+    return arr
 }
 
 removeElement(array, 5 );
