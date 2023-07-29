beeb635d1ac7759fa221c674470c8f0d0cdd108f
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-07-30 00:27:45 +0300

    first push

--- js/script.js
+++ js/script.js
@@ -1,9 +1,48 @@
-const changeNumber = function (number){
-    return number.match(/380/) === null ? null : number.replace(/\D/g, '').replace(/^38/, '')
+const str = "The dates between 1976-03-12, and 1983-10-14, the code was 2004-67-12 and possible invalid date 8765-11-34";
+
+const findDates = function (someText){
+  const year = 'year'
+  const month = 'month'
+  const day = 'day';
+  const monthName = 'monthName'
+
+  const monthCount= {
+    January: '01',
+    February: '02',
+    March: '03',
+    April: '04',
+    May: '05',
+    June: '06',
+    July: '07',
+    August: '08',
+    September: '09',
+    October: '10',
+    November: '11',
+    December: '12'
+  }
+
+  let newStr = null;
+
+  newStr = str.match(/(?<year>\d{4})-(?<mounth>\d{2})-(?<day>\d{2})/g).map((item)=>{
+    const arr = item.split('-');
+
+    for (let key in monthCount){
+
+      if (arr[1] === monthCount[key]){
+        return {
+          [year]: arr[0],
+          [month]: arr[1],
+          [monthName]: key,
+          [day]: arr[2]
+        }
+      }
+    }
+
+  })
+
+  newStr.splice(newStr.indexOf(undefined),1)
+  return newStr
 }
 
-console.log(`Phone number was "380 44 364 83 65" became ${changeNumber('380 44 364 83 65')}`)
-console.log(`Phone number was "+380 44 364 83 65" became ${changeNumber('+380 44 364 83 65')}`)
-console.log(`Phone number was "380-44-364-83-65" became ${changeNumber('380-44-364-83-65')}`)
-console.log(`Phone number was "+380-44-364-83-65" became ${changeNumber('+380-44-364-83-65')}`)
-console.log(`Phone number was "+38 044 364 83 65" became ${changeNumber('+38 044 364 83 65')}`)
+console.log(findDates(str))
+
