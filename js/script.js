6bb3f4dd23ab4055ab67599e01d6993f7884a126
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-08-16 20:54:09 +0300

    first push

--- js/script.js
+++ js/script.js
@@ -1,77 +1,95 @@
-//Дан масив об'єктів. Вивести масив телефонних номерів користувачів, у яких баланс більше 2000 доларів. І знайти суму всіх балансів користувачів
-let users = [
-    {
-        "index": 0,
-        "isActive": true,
-        "balance": "$2,226.60",
-        "name": "Eugenia Sawyer",
-        "gender": "female",
-        "phone": "+1 (840) 583-3207",
-        "address": "949 John Street, Rose, Puerto Rico, 1857"
-    },
-    {
-        "index": 1,
-        "isActive": true,
-        "balance": "$2,613.77",
-        "name": "Pauline Gallegos",
-        "gender": "female",
-        "phone": "+1 (985) 593-3328",
-        "address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-    },
-    {
-        "index": 2,
-        "isActive": false,
-        "balance": "$3,976.41",
-        "name": "Middleton Chaney",
-        "gender": "male",
-        "phone": "+1 (995) 591-2478",
-        "address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-    },
-    {
-        "index": 3,
-        "isActive": true,
-        "balance": "$1,934.58",
-        "name": "Burns Poole",
-        "gender": "male",
-        "phone": "+1 (885) 559-3422",
-        "address": "730 Seba Avenue, Osage, Alabama, 6290"
-    },
-    {
-        "index": 4,
-        "isActive": true,
-        "balance": "$3,261.65",
-        "name": "Mcfadden Horne",
-        "gender": "male",
-        "phone": "+1 (942) 565-3988",
-        "address": "120 Scholes Street, Kirk, Michigan, 1018"
-    },
-    {
-        "index": 5,
-        "isActive": false,
-        "balance": "$1,790.56",
-        "name": "Suzette Lewis",
-        "gender": "female",
-        "phone": "+1 (837) 586-3283",
-        "address": "314 Dunne Place, Bawcomville, Guam, 9053"
+class Student {
+    name = '';
+    lastName = '';
+    yearOfBirth = '';
+    arrEstimates = [];
+    constructor(name, lastName, yearOfBirth, estimates) {
+        this.name = name;
+        this.lastName = lastName;
+        this.yearOfBirth = yearOfBirth;
+        this.arrEstimates = estimates
     }
-]
 
-const newUser = users.slice()
-newUser.forEach((item) => {item.balance = item.balance.replace(/[^0-9.]/g, '')})
-
-let numberPhones =[]
-
-newUser.forEach(item=> {
-    if (item.balance > 2000){
-        return  numberPhones.push(item.phone)
+    getStudentAge (){
+        const date = new Date()
+        return `Вам ${date.getFullYear() - this.yearOfBirth} років`
     }
-})
-console.log(numberPhones)
 
+    averageScore(){
+    const overallSumEstimate = this.arrEstimates.reduce((sum, item)=> sum +=item,0)
+        return overallSumEstimate / this.arrEstimates.length
+    }
 
-const result = newUser.reduce((acc, {balance})=> {
-    acc += +balance
-    return acc
-}, 0)
+    isAttending = new Array(25).fill('');
 
-console.log(`Сума всіх балансів користувача ${result.toFixed(2)}`)
+    present(count){
+        const counter = count - 1
+        for (let i = 0; i < counter; i++){
+                this.present()
+        }
+        const indexAttending = this.isAttending.findIndex(item => item === '')
+        this.isAttending[indexAttending] = true
+
+        return this.isAttending
+    }
+
+    absent(count){
+        const counter = count - 1
+        for (let i =0; i < counter; i++){
+            this.absent()
+        }
+        const indexAttending = this.isAttending.findIndex(item => item === '')
+        this.isAttending[indexAttending] = false
+
+        return this.isAttending
+    }
+
+    averageAttending(){
+        const overallAverageAttending = this.isAttending.reduce((sum, item)=> {
+            const convertInNumber = +item
+                return sum += convertInNumber
+        },0)
+        return overallAverageAttending  / this.isAttending.length
+    }
+
+    summary(){
+        if (this.averageScore() > 90 && this.averageAttending() > 0.9 ){
+            console.log('Молодець!')
+        }else if (this.averageScore() > 90 || this.averageAttending() > 0.9){
+            console.log('Добре, але можна краще')
+        }else if (this.averageScore() < 90 && this.averageAttending() < 0.9){
+            console.log('Редиска')
+        }
+    }
+
+}
+
+const pointsForStudent1 = [0, 100, 98, 100, 99, 90, 80, 95,90, 100, 63, 99, 100, 100, 90, 100, 66, 100, 97, 96, 99, 92, 94, 87, 95]
+
+const student1 = new Student('Dima', 'Foliushniak', '1994', pointsForStudent1)
+
+console.log(student1.getStudentAge());
+console.log(student1.averageScore());
+student1.present(23);
+student1.absent(2);
+student1.averageAttending();
+student1.summary();
+
+const pointsForStudent2 = [100, 100, 98, 100, 99, 90, 80, 95,90, 100, 99, 99, 100, 100, 90, 100, 98, 100, 97, 96, 99, 92, 94, 87, 95]
+
+const student2 = new Student('Nikola', 'Chashechkin', '2000', pointsForStudent2)
+console.log(student2.getStudentAge());
+console.log(student2.averageScore());
+student2.present(25);
+student2.averageAttending();
+student2.summary();
+
+const pointsForStudent3 = [60, 100, 98, 50, 99, 90, 80, 95,90, 80, 99, 99, 64, 100, 90, 66, 98, 100, 97, 96, 99, 92, 94, 87, 95]
+
+const student3 = new Student('Nikola', 'Chashechkin', '2000', pointsForStudent3)
+console.log(student3.getStudentAge());
+console.log(student3.averageScore());
+student3.present(20);
+student1.absent(5);
+student3.averageAttending();
+student3.summary();
