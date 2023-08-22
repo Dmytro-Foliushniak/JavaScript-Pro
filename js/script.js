642774f8b89e75c7e0780a17bef02c1cad761cd6
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-08-23 00:56:17 +0300

    first push

--- js/script.js
+++ js/script.js
@@ -1,17 +1,45 @@
-const inputText = document.querySelector('#inputText');
-inputText.style.marginRight = '10px';
+const btnNext = document.querySelector('.btn-next');
+const btnBack = document.querySelector('.btn-back');
+const image = document.querySelectorAll('.image');
 
-const box = document.querySelector('.box');
-box.style.display = 'flex';
+const slider = function (back,next){
+    let i = 0;
 
-const div = document.createElement('div');
-div.innerHTML = 'Some Text';
-box.append(div);
-div.hidden = true;
+    btnBack.hidden = true;
 
-inputText.addEventListener('focus', ()=> {
-div.hidden = false;
-});
-inputText.addEventListener('focusout', ()=> {
-    div.hidden = true;
-});
+    const operationWithClass = function (arg){
+        if (image[i].classList.contains('active')){
+            image[i].classList.remove('active');
+            image[i].hidden = true;
+        }
+        if (arg === 'plus'){
+            i++;
+        }
+        if (arg === 'minus'){
+            i--;
+        }
+
+        image[i].classList.add('active');
+        image[i].hidden = false;
+    };
+
+    next.addEventListener('click', ()=>{
+        btnBack.hidden = false;
+        operationWithClass('plus');
+
+        if (image.item(image.length-1) === image[i]){
+            btnNext.hidden = true;
+        }
+    });
+
+    back.addEventListener('click', ()=>{
+        btnNext.hidden = false;
+        operationWithClass('minus');
+
+        if (image.item(0) === image[i]){
+            btnBack.hidden = true;
+        }
+    });
+};
+
+slider(btnBack,btnNext);
