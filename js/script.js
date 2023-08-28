e4a6d2af248ce8b259cb0eebc652d4f4256efed7
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-08-28 23:32:18 +0300

    change visual

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
-const emoji = document.querySelector('.emoji')
-const emojiBox = document.querySelectorAll('.emoji-box')
-console.log(emojiBox)
 
-emoji.addEventListener('click', ()=>{
-    const element = event.target
-    for (let item of emojiBox){
-       if (item.firstElementChild===element){
 
-           let value = item.lastElementChild.getAttribute('value')
-           item.lastElementChild.setAttribute('value', `${+value + 1}`)
-       }
-    }
-})
+const votingEmoji = function (){
+    const emoji = document.querySelector('.emoji')
+    const emojiBox = document.querySelectorAll('.emoji-box')
+
+    emoji.addEventListener('click', ()=>{
+        const element = event.target
+        for (let item of emojiBox){
+            if (item.firstElementChild===element){
+                let value = item.lastElementChild.getAttribute('value')
+                item.lastElementChild.setAttribute('value', `${+value + 1}`)
+            }
+        }
+    })
+}
+
+votingEmoji()
+
+
