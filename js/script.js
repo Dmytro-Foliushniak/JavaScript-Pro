ca1f42dee100ce0d4138c4da40f3e0d090da91c4
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-08-28 17:52:43 +0300

    first push

--- js/script.js
+++ js/script.js
@@ -1,20 +1,14 @@
+const emoji = document.querySelector('.emoji')
+const emojiBox = document.querySelectorAll('.emoji-box')
+console.log(emojiBox)
 
+emoji.addEventListener('click', ()=>{
+    const element = event.target
+    for (let item of emojiBox){
+       if (item.firstElementChild===element){
 
-const createTable = function (countTR, countTD){
-    const tableContainer = document.querySelector('.table-box');
-    const table = document.createElement('table');
-    table.setAttribute('class', 'table')
-    tableContainer.prepend(table);
-
-    for (let i = 0; i < countTR; i++){
-        table.innerHTML += `<tr class="tr"></tr>`
-        let tr = document.querySelectorAll('tr')
-        let sum = 0
-        for (let j = 0; j < countTD; j++){
-            tr[i].innerHTML += `<td class="td">${sum +=i+1}</td>`
-        }
+           let value = item.lastElementChild.getAttribute('value')
+           item.lastElementChild.setAttribute('value', `${+value + 1}`)
+       }
     }
-
-}
-
-createTable(10,10)
+})
