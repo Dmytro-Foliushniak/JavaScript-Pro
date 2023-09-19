6956e3a7e294a49052f015eae07966df3be6a53f
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-09-19 15:51:41 +0300

    fix form logic

---
+++ js/validation.js
@@ -0,0 +1,52 @@
+const validForm = function (thisElement, productsOfArray, nameArticle){
+    const orderMassage = document.createElement('div')
+    const mainWrapper = document.querySelector('.main');
+    orderMassage.className = 'massage'
+
+    let orderText = ''
+
+    const checkButton = document.querySelector('.checkoutButton')
+
+    const data = {}
+
+        for (let element of thisElement) {
+            if (element.value !== ''){
+                if (element.checked) {
+                    data[element.name] = element.value
+                }
+                if (element.type !== 'radio' && element.type !== 'submit') {
+                    data[element.name] = element.value
+                }
+                localStorage.setItem('DATA_ITEM', JSON.stringify(data))
+
+                productsOfArray.forEach(item=>{
+                    if (item.aboutProduct.article === nameArticle){
+                        orderText = `<p>${data['fullName']} Ви замовили ${item.name} по ціні ${item.price} на відділенна ${data['warehouse']}</p>`
+                    }
+                })
+                orderMassage.innerHTML = orderText
+                mainWrapper.append(orderMassage)
+                checkButton.setAttribute('disabled', 'true')
+            }
+        }
+}
+
+const invalidForm = function (thisElement){
+    const errorMassage = document.createElement('div');
+    errorMassage.className = "massage";
+
+    let errorText = ''
+
+    for (let element of thisElement) {
+        if (element.validity.valueMissing) {
+            errorText += `<p>Заповніть будь-ласка поле: ${element.name}</p>`;
+        }
+        errorMassage.innerHTML = errorText
+        document.querySelector('.main').append(errorMassage);
+        errorMassage.insertAdjacentHTML('beforeend', `<input class="buttonFromErrorForm" type="button" value="OK" >`)
+        const buttonFromErrorMassage = document.querySelector('.buttonFromErrorForm')
+        buttonFromErrorMassage.addEventListener('click', () => {
+            errorMassage.remove()
+        })
+    }
+}
