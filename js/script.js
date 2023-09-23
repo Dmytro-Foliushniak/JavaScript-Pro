aa2e6f1d6f70f579f7f7052ca2f5df2f9443c7fd
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-09-23 19:16:17 +0300

    frist push

--- js/script.js
+++ js/script.js
@@ -1,46 +1,52 @@
+const input = document.querySelector('input');
+const btn = document.querySelector('.btn');
 
-const weatherBox = document.querySelector('.weather')
-const getWeatherAPI = async function (){
-    const URL_API = 'http://api.openweathermap.org/data/2.5/weather?q=LVIV&units=metric&APPID=5d066958a60d315387d9492393935c19'
+const getPostsAPI = async function (){
 
-    try {
-        const response =  await fetch(URL_API)
-        const weatherData = await response.json()
-        getWeather(weatherData)
-    }catch (error){
-        console.log(error)
+    const DATA_API = fetch('https://jsonplaceholder.typicode.com/posts');
+    const response = await DATA_API;
+    const posts = await response.json();
+
+    getPost(posts);
+}
+
+const getPost = function (data){
+    const main = document.querySelector('.main');
+    const postBlock = document.createElement('div');
+    postBlock.className = 'post-block';
+    let patternPost = '';
+    data.forEach(item=>{
+        if (+input.value === item.id){
+            patternPost = `
+            <h3>Ваш пост</h3>
+            <h4 class="post-title">${item.title}</h4>
+            <p class="post-text">${item.body}</p>
+            <button class="btn-comment">Комментар</button>
+            <hr>`
+        }
+    });
+    postBlock.innerHTML = patternPost;
+    main.append(postBlock);
+
+    if (postBlock){
+        const comment = document.querySelector('.btn-comment');
+        comment.addEventListener('click', (event)=>{
+            comment.remove();
+            postBlock.insertAdjacentHTML('beforeend', `<div class="comment-box">
+            <h4>Ваш комментар</h4>
+            <textarea class="comment"></textarea>
+            <button type="submit">Відправити</button>
+            <button type="submit">Відміна</button>
+            </div>`);
+        });
     }
-}
-
-if (weatherBox){
-    getWeatherAPI();
-}
-
-const getWeather = function (data){
-
-    const city = data.name;
-    const temp = data.main.temp;
-    const pressure = data.main.pressure
-    const description = data.weather[0].description;
-    const humidity = data.main.humidity;
-    const speed = data.wind.speed;
-    const deg = data.wind.deg;
-    const icon = data.weather[0].icon
-
-    const weatherPattern = `<div class="weather-header">
-            <p class="weather-city">${city}</p>
-            <div class="weather-icon">
-                <img src="http://openweathermap.org/img/w/${icon}.png" alt="icon">
-            </div>
-        </div>
-        <ul class="weather-param">
-            <li class="weather-item">Температура: ${temp}</li>
-            <li class="weather-item">Тиск: ${pressure} </li>
-            <li class="weather-item">Вологість: ${humidity}</li>
-            <li class="weather-item">Швидкість вітру: ${speed} </li>
-            <li class="weather-item">Напрямок у градусах: ${deg} </li>
-            <li class="weather-item">Опис:${description} </li>
-        </ul>`
-
-    weatherBox.innerHTML = weatherPattern
-}
+};
+btn.addEventListener('click', (event)=>{
+    event.preventDefault();
+    if ((!input.validity.rangeUnderflow && !input.validity.rangeOverflow) && !input.validity.valueMissing){
+        getPostsAPI();
+    }
+    if (document.querySelector('.post-block')){
+        document.querySelector('.post-block').remove();
+    }
+});
