03b423f1f707b2346b104c47479281db8e10d5fb
Dmitro Foliushnial <drunkluffy@gmail.com>
2023-10-17 14:21:54 +0300

    fix input winners

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -1,34 +1,36 @@
 import SmileVoice from "./components/SmileVoice";
-import { useState } from 'react';
+import {useState} from 'react';
 import '../src/App.css'
 
 
 function App() {
     const [winner, setWinner] = useState(null)
+    const [elem, setElement] = useState('')
 
-    const showResult = () => {
-        const emoji = document.querySelectorAll('.emoji-list')
+     function updateValue(elem){
+        setElement(elem)
+    }
+
+    function showResult(){
         let maxVotes = null;
         let winnerId = null;
-        emoji.forEach((item, index)=>{
-            const votes = +item.querySelector('.voting').value
-            if (votes > maxVotes){
-                maxVotes = votes;
-                winnerId = index + 1
+            const votesT = elem.value
+            if (votesT > maxVotes){
+                maxVotes = votesT
+                winnerId = elem
             }
-        })
-        if (winnerId >= 1) {
-            const winnerHTML = emoji[winnerId-1].innerHTML
+        if (winnerId) {
+            const winnerHTML = winnerId.parentNode.innerHTML
             setWinner(winnerHTML);
         }
     }
   return (
     <div className="App">
         <ul className="emoji">
-            <SmileVoice emoji={1}/>
-            <SmileVoice emoji={2}/>
-            <SmileVoice emoji={3}/>
-            <SmileVoice emoji={4}/>
+            <SmileVoice onChange ={updateValue} emoji={1}/>
+            <SmileVoice onChange ={updateValue} emoji={2}/>
+            <SmileVoice onChange ={updateValue} emoji={3}/>
+            <SmileVoice onChange ={updateValue} emoji={4}/>
         </ul>
         <button className="btn-winner" onClick={showResult}>Show Results</button>
         {winner ? (
--- react-project/src/components/SmileVoice.jsx
+++ react-project/src/components/SmileVoice.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
 import '../styles/emoji.css'
-function SmileVoice ({emoji}){
+function SmileVoice ({onChange, emoji}){
     const [count, setCount] = useState(0);
-    function countClick() {
+
+    function countClick(e) {
+        let newValue = e.target.nextSibling;
+        onChange(newValue);
         setCount(count + 1);
     }
 
+
+
     return (
             <li className="emoji-list">
-                <input className="emoji-img" type="image" src={`/img/${emoji}.jpg`} alt="image" onClick={countClick}/>
+                <input className="emoji-img" type="image" src={`/img/${emoji}.jpg`} alt="image" onClick={(e)=>{countClick(e) }}/>
                 <input className="voting" type="text" name="" id="" disabled value={count}/>
             </li>
         )
